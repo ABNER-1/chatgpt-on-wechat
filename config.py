6037e6447846dbb8e75ab5855987c1e7d017c573
Abner-1 <abner199709@gmail.com>
2023-03-31 00:00:08 +0800

    support assign config prefix in env

--- config.py
+++ config.py
@@ -103,7 +103,8 @@ config = Config()
 
 def load_config():
     global config
-    config_path = "./config.json"
+    prefix = os.environ.get('CONFIG_PREFIX', ".")
+    config_path = prefix + "/config.json"
     if not os.path.exists(config_path):
         logger.info('配置文件不存在，将使用config-template.json模板')
         config_path = "./config-template.json"
